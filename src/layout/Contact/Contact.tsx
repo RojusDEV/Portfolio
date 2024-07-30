387528c48b0b17ac0678e16baa81a979a658e0a1
Roy <rojusbalciunas@gmail.com>
2024-07-30 20:44:00 +0300

    refactor

--- src/layout/Contact/Contact.tsx
+++ src/layout/Contact/Contact.tsx
@@ -1,33 +1,64 @@
-import { FormEvent, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { FaFacebook, FaInstagram, FaSms } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
 import "./Contact.scss";
 import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import z from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 const Contact = () => {
   const form = useRef<HTMLFormElement>(null);
   const [showConfetti, setShowConfetti] = useState(false);
+  const schema = z.object({
+    user_email: z
+      .string()
+      .min(1, { message: "Email should be atleast 1 character long" })
+      .max(100, { message: "Email should be bellow 100 characters long" })
+      .email(),
+    user_name: z
+      .string()
+      .min(1, { message: "Name should be atleast 1 character long" })
+      .max(100, { message: "Name should be bellow 100 characters long" }),
+    message: z
+      .string()
+      .min(20, { message: "message should be atleast 20 characters long" })
+      .max(5000, { message: "Message should be bellow 5000 characters long" }),
+  });
 
-  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if(form.current) {
+  type schemaType = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<schemaType>({ resolver: zodResolver(schema) });
+  const sendEmail: SubmitHandler<schemaType> = (data) => {
+    console.log(data);
+    if (form.current) {
       emailjs
-        .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
-          publicKey: import.meta.env.VITE_PUBLIC_KEY,
-        })
+        .sendForm(
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
+          form.current,
+          {
+            publicKey: import.meta.env.VITE_PUBLIC_KEY,
+          }
+        )
         .then(
           () => {
             setShowConfetti(true);
-            console.log('SUCCESS!');
+            console.log("SUCCESS!");
           },
           (error) => {
             setShowConfetti(false);
-            console.log('FAILED...', error.text);
-          },
+            console.log("FAILED...", error.text);
+          }
         );
     }
   };
 
+  console.log(errors);
   return (
     <section className="contactMe">
       <div className="contactMe__wrapper">
@@ -52,10 +83,33 @@ const Contact = () => {
           </ul>
         </div>
         <div className="col">
-          <form ref={form} onSubmit={sendEmail}>
-            <input type="text" name="user_name" placeholder="Name: " autoComplete="true" />
-            <input type="email" name="user_email" placeholder="Email: " autoComplete="true"/>
-            <textarea name="message" placeholder="Message: " />
+          <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+            <input
+              type="text"
+              {...register("user_name")}
+              placeholder="Name: "
+            />
+            {errors.user_name && (
+              <span className="form-error-message">
+                {errors.user_name?.message}
+              </span>
+            )}
+            <input
+              type="email"
+              {...register("user_email")}
+              placeholder="Email: "
+            />
+            {errors.user_email && (
+              <span className="form-error-message">
+                {errors.user_email?.message}
+              </span>
+            )}
+            <textarea placeholder="Message: " {...register("message")} />
+            {errors.message && (
+              <span className="form-error-message">
+                {errors.message.message}
+              </span>
+            )}
             <input type="submit" value="Send" />
           </form>
         </div>
